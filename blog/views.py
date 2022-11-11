c89ae308126763e9194b61e7728eefadc3534e97
candiceevemiller <candice.eve.miller@gmail.com>
2022-11-11 13:06:53 -0500

    integrated db

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Candice',
-        'title': 'Blog Post 1',
-        'content': 'First Post!',
-        'date_posted': 'November 10th, 2022'
-    },
-    {
-        'author': 'The Dude',
-        'title': 'The Dude Abides',
-        'content': 'I\'m abiding!',
-        'date_posted': 'November 10th, 2022'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
